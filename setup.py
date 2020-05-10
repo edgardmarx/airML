1192f1886f51c05c1eddb66a49229401a0213cfe
LahiruOshara <oshara.16@cse.mrt.ac.lk>
2020-05-10 22:17:20 +0530

    git push -m "setup changes"

--- setup.py
+++ setup.py
@@ -1,25 +1,21 @@
 from setuptools import setup
 
-# with open("README.md", "r") as fh:
-#     long_description = fh.read()
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="airML", # Replace with your own username
+    name="airML", 
     version="0.0.1",
-    author="Edgard Marx",
+    author="Lahiru Oshara Hinguruduwa",
     description="application will allow users to " +
                 "share and dereference ML models.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    # packages=setuptools.find_packages(),
-    # classifiers=[
-    #     "Programming Language :: Python :: 3",
-    #     "License :: OSI Approved :: MIT License",
-    #     "Operating System :: OS Independent",
-    # ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Apache License :: 2.0",
+        "Operating System :: OS Independent",
+    ],
     py_modules= ['airML'],
     packages = [""],
     package_dir = {'':'src'},
